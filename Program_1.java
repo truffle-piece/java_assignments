e2ea8191ae97f83711400467b8b958a275352367
truffle-piece <80003820+truffle-piece@users.noreply.github.com>
2022-07-07 11:04:06 +0300

    Убрал переменную с пробелом

--- Program_1.java
+++ Program_1.java
@@ -4,8 +4,7 @@ public class Program_1 {
 	public static void main(String[] args) {
 		
 		int a=3,b=6 ,c=5;
-		String str = "  ";
-		System.out.println(a+str+b+str+c);
+		System.out.println(a+"  "+b+"  "+c);
 
 	}
 
